0b6daed6abfc667516411675895e19bedd2380d4
Brian Cusack <profess78@hotmail.com>
2019-03-10 20:55:21 +1100

    auth gaurd

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PasswordlessAuthComponent } from './passwordless-auth/passwordless-auth.component';
+import { AuthGuard } from './auth.guard';
 
 const routes: Routes = [
   { path: '', loadChildren: './home/home.module#HomePageModule' },
---
+++ src/app/auth.guard.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+  constructor(private router: Router) { }
+
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): boolean {
+
+    const loggedIn = false; // replace with actual user auth checking logic
+
+    if (!loggedIn) {
+      this.router.navigate(['/']);
+    }
+
+    return loggedIn;
+  }
+}
