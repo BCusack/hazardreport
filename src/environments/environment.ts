2c46bca0935f60b775fd1f7bf0a26a13e74ddd99
Brian Cusack <briancusack78@gmail.com>
2019-04-10 17:31:40 +1000

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,12 +5,12 @@
 export const environment = {
   production: false,
   firebaseConfig: {
-    apiKey: "AIzaSyAhGNN-Ask_CzJ3-rS-6zkVgEx1mC-K7CM",
-    authDomain: "hazardreport-13d67.firebaseapp.com",
-    databaseURL: "https://hazardreport-13d67.firebaseio.com",
-    projectId: "hazardreport-13d67",
-    storageBucket: "hazardreport-13d67.appspot.com",
-    messagingSenderId: "36596629309"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: ""
   },
   redirect: "https://hazardreport-13d67.firebaseapp.com/login"
 };
